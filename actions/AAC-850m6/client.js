4d036333ef22133c5e06f071d358173ccf80b065
Karim Ardalan <karim@misinc.net>
2021-07-30 14:35:38 -0600

    Automatic commit from Bubble

---
+++ actions/AAC-850m6/client.js
@@ -0,0 +1,17 @@
+function(properties, context) {
+
+
+  //Load any data 
+
+
+  function find_sha1() {
+    var text = sha1($('#inputtextarea').val()); 
+    $('#outputtextarea').val(text);
+  }
+
+
+  //Do the operation
+
+
+
+}
